cf37d418dd094f616fd2a87baef293457e98b6b9
thevicraft <thevicraft911@gmail.com>
2021-10-31 14:28:36 +0100

    Test

--- src/com/thevicraft/calculator/Main.java
+++ src/com/thevicraft/calculator/Main.java
@@ -6,7 +6,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		GuiTaschenrechner taschenrechner = new GuiTaschenrechner("Taschenrechner"/*,"/"*/);
-		//System.out.println(SimpleMath.logarithm((double) (45),(double) (8.0)));
+		
 		
 		
 
